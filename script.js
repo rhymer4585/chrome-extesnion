35261a8546e539c4d47c0c0c9995492f83c96e15
Ayush <rhymer4585@gmail.com>
2024-07-25 20:07:53 +0530

    initial files

---
+++ script.js
@@ -0,0 +1,8 @@
+fetch('https://icanhazdadjoke.com/slack')
+.then(data=>data.json())
+.then(jokedata=>{
+
+    const jokeText=jokedata.attachments[0].text;
+    const jokeElement=document.getElementById('jokeElement')
+    jokeElement.innerHTML=jokeText;
+})
